1703707deeb5bf4caf3da38f3d1b03d1ad053590
Enrique Garcia <enriquegc.1991@gmail.com>
2025-01-17 20:01:22 +0100

    Correccion Home-Page.ts, el getter estaba haciendo una funcion en vez de ser un getter

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,20 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { GifsService } from '../../services/gifs.service';
 import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-home-page',
-  templateUrl: `./home-page.component.html`,
   standalone: false,
-  //styleUrl: './home-page.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
 
-  constructor (private gifsService: GifsService) {}
+  constructor( private gifsService: GifsService ) {}
 
   get gifs(): Gif[] {
-    return this.gifsService.gifList
+    return this.gifsService.gifList;
   }
 
- }
+}
